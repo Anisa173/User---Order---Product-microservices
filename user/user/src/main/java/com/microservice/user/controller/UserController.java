a1691332dfbc18ba97efd41f21b748523b23f096
Anisa173 <celaanisa07@gmail.com>
2025-10-10 16:11:45 +0200

    jhds

--- user/user/src/main/java/com/microservice/user/controller/UserController.java
+++ user/user/src/main/java/com/microservice/user/controller/UserController.java
@@ -6,6 +6,7 @@ import com.microservice.user.dto.UserResponse;
 import com.microservice.user.model.User;
 import com.microservice.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,30 +15,36 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/us")
+    @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
     public List<UserResponse> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    @PostMapping("/user")
+    @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public List<UserResponse> addNewOne(@RequestBody UserRequest us) {
         return Collections.singletonList(userService.addNewOne(us));
     }
 
-    @GetMapping("/users/{userId}")
-    public UserResponse getUserById(@PathVariable("userId") Integer userId) {
+    @GetMapping("/{userId}")
+    public UserResponse getUserById(@PathVariable("userId") Integer userId)
+    {  log.info("Request received foe user with Id:" + userId);
+       log.trace("This is Trace level--Used for very detailed logs ");
+       log.debug("This is Debug level --- Used for development debugging");
+       log.warn("This is Warn level ---Something might have happend but it's not critical");
+       log.error("This is ERROR Level---Somthing failures");
         return userService.getUSERById(userId);
     }
 
-    @PutMapping("/userModified/{userId}")
+    @PutMapping("/{userId}")
     public ResponseEntity<String> modifyUser(@PathVariable("userId") Integer userId, @RequestBody UserResponse user) {
         boolean updated = userService.modifyUser(user, userId);
         if (updated) {
