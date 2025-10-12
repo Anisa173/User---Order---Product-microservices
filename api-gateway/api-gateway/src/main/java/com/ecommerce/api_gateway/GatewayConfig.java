2b2e6019cb140a40ff51766374a5045e01b66a01
Anisa173 <celaanisa07@gmail.com>
2025-10-12 11:51:37 +0200

    jykdhf

---
+++ api-gateway/api-gateway/src/main/java/com/ecommerce/api_gateway/GatewayConfig.java
@@ -0,0 +1,39 @@
+package com.ecommerce.api_gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route("product-service", r -> r
+                        .path("/api/products/**")
+                        .uri("lb://PRODUCT-SERVICE"))
+                .route("user-service", r -> r
+                        .path("/api/users/**")
+                        .uri("lb://USER-SERVICE"))
+                .route("order-service", r -> r
+                        .path("/api/order/**", "/api/cartitem/**")
+                        .uri("lb://ORDER-SERVICE"))
+                .route("eureka-server", r -> r
+                        .path("/eureka/main")
+                        .filters(f -> f.rewritePath("/eureka/main", "/"))
+                        .uri("http://localhost:8761"))
+                .route("eureka-server-static", r -> r
+                        .path("/eureka/**")
+                        .uri("http://localhost:8761"))
+                .build();
+
+
+    }
+
+
+}
